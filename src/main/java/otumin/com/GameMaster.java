82eaf2c05b5f48eb3f97b4d7d5602ab103d54172
masuda-kenichi <masuda_kenichi@cyberagent.co.jp>
2014-05-23 00:40:42 +0900

    全ユーザへの配布部分をリファクタリング

--- src/main/java/otumin/com/GameMaster.java
+++ src/main/java/otumin/com/GameMaster.java
@@ -1,6 +1,5 @@
 package otumin.com;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,17 +32,11 @@ public class GameMaster {
         // ユーザ作成
         um.createMultipleUser(usersNum);
 
-        // 作成したユーザにカード配布
-        for(int i = 0; i < usersNum; i++){
-            User user = um.getUser(i);
-            for(int j = 0; j < 10; j++){ // 10枚配布
-                Card c = deck.getCardByFirst();
-                user.receiveCard(c);
-                deck.removeCardByFirst();
-            }
-        }
+        // 作成した全ユーザにカード配布
+        distributeCardAllUser(usersNum);
 
         // ここまででユーザには配り終わってる。
+        // ので、その状態の山札の情報を表示する
         System.out.println("----------------");
         System.out.println("ユーザへの配布後の山札の状態");
         System.out.println("");
@@ -55,14 +48,13 @@ public class GameMaster {
         System.out.println("----------------");
 
 
-
-
         // 試験的にユーザの手札を全て表示する
         for(int i = 0; i < usersNum; i++){
             System.out.println("#########################");
             um.showHandsByUserIndex(i);
         }
 
+        // 各レーンに残りの札を配置する（ニムトではレーン1枚ずつ計4枚配置することになっている）
         Field field = new Field();
         field.receivePutCardLane1(deck.getCardByFirst());
         deck.removeCardByFirst();
@@ -76,6 +68,7 @@ public class GameMaster {
         field.receivePutCardLane4(deck.getCardByFirst());
         deck.removeCardByFirst();
 
+        // ここまでで全ユーザと全列にカードの配置が終わってるので、デッキの中身を表示してみる
         System.out.println("----------------");
         System.out.println("最終的なデッキの中身:");
         System.out.println("");
@@ -84,6 +77,17 @@ public class GameMaster {
         }
         System.out.println("");
         System.out.println("----------------");
+    }
+
+    private void distributeCardAllUser(int usersNum){
+        for(int i = 0; i < usersNum; i++){
+            User user = um.getUser(i);
+            for(int j = 0; j < 10; j++){ // 10枚配布
+                Card c = deck.getCardByFirst();
+                user.receiveCard(c);
+                deck.removeCardByFirst();
+            }
+        }
     }
 
 
