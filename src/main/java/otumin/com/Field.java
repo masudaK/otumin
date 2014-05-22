c41ed7bae3017cb0b74e79824a7b9c6d88535654
masuda-kenichi <masuda_kenichi@cyberagent.co.jp>
2014-05-22 12:15:50 +0900

    レーンの処理追加

--- src/main/java/otumin/com/Field.java
+++ src/main/java/otumin/com/Field.java
@@ -12,13 +12,19 @@ import java.util.List;
  */
 public class Field {
 
-    private List<Card> lane;
+    private List<Card> lane1;
+    private List<Card> lane2;
+    private List<Card> lane3;
+    private List<Card> lane4;
 
     public Field(){
-        this.lane = new ArrayList<Card>();
+        this.lane1 = new ArrayList<Card>();
+        this.lane2 = new ArrayList<Card>();
+        this.lane3 = new ArrayList<Card>();
+        this.lane4 = new ArrayList<Card>();
     }
 
     public void receiveCard(Card card) {
-        lane.add(card);
+        lane1.add(card);
     }
 }
