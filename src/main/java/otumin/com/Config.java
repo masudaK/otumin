bb0b33792058b2bc3b63c685f2b120b2cb6a4ebe
masuda-kenichi <masuda_kenichi@cyberagent.co.jp>
2014-06-10 01:19:10 +0900

    マジックナンバーをConfigクラスに外出し

--- src/main/java/otumin/com/Config.java
+++ src/main/java/otumin/com/Config.java
@@ -16,6 +16,6 @@ public class Config {
     public static final int OWN_USER_INDEX = 0;
     public static final String START_MESSAGE = "Nimto Start!!";
     public static final String INPUT_ERROR_MESSAGE = "入力時にエラーが発生しました";
-    public static int MIN_USERS_NUM = 2;
-    public static int MAX_USERS_NUM = 10;
+    public static final int MIN_USERS_NUM = 2;
+    public static final int MAX_USERS_NUM = 10;
 }
