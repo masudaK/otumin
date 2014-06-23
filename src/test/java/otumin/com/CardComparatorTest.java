26c3fc674b4abee3c5422c2379bc3997351cc4f2
masuda-kenichi <masuda_kenichi@cyberagent.co.jp>
2014-06-24 00:52:16 +0900

    CardComparatorのテスト追加

---
+++ src/test/java/otumin/com/CardComparatorTest.java
@@ -0,0 +1,22 @@
+package otumin.com;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: masuda_kenichi
+ * Date: 14/06/24
+ * Time: 0:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class CardComparatorTest {
+
+    @Test
+    public void testCompare() throws Exception {
+        CardComparator cc = new CardComparator();
+        Card cardA = new Card(1,1);
+        Card cardB = new Card(2,1);
+        assertEquals(-1, cc.compare(cardA, cardB));
+    }
+}
